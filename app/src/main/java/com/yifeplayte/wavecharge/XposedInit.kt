ca2d31eb88fd71c7cd3022723f0992eaf5bba740
YifePlayte <yife-playte@foxmail.com>
2022-02-23 15:47:04 +0800

    Initial commit

---
+++ app/src/main/java/com/yifeplayte/wavecharge/XposedInit.kt
@@ -0,0 +1,43 @@
+package com.yifeplayte.wavecharge
+
+import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
+import com.github.kyuubiran.ezxhelper.utils.findMethod
+import com.github.kyuubiran.ezxhelper.utils.hookMethod
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class XposedInit : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        EzXHelperInit.initHandleLoadPackage(lpparam)
+        when (lpparam.packageName) {
+            "com.android.systemui" ->
+                supportWaveChargeAnimationHook(lpparam)
+        }
+    }
+    private fun supportWaveChargeAnimationHook(lpparam: XC_LoadPackage.LoadPackageParam) {
+        findMethod("com.android.keyguard.charge.ChargeUtils") {
+            name == "supportWaveChargeAnimation"
+        }.hookMethod {
+            after { param ->
+                var ex = Throwable()
+                var stackElement = ex.stackTrace
+                var mResult = false
+                val classTrue = setOf(
+                    "com.android.keyguard.charge.ChargeUtils",
+                    "com.android.keyguard.charge.container.MiuiChargeContainerView",
+                    "com.android.keyguard.charge.view"
+                )
+                for(i in stackElement.indices) {
+                    //XposedBridge.log("Dump Stack " + i + ": " + stackElement[i].className + "--" + stackElement[i].methodName)
+                    when {
+                        stackElement[i].className in classTrue -> {
+                            mResult = true
+                            break
+                        }
+                    }
+                }
+                param.result = mResult
+            }
+        }
+    }
+}
